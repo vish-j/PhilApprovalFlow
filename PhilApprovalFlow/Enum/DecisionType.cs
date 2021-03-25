842cd1ca61874d2276e819fad1f23931099153f7
vishj <contact@vishj.com>
2021-03-25 22:55:58 +0400

    enum comments added

--- PhilApprovalFlow/Enum/DecisionType.cs
+++ PhilApprovalFlow/Enum/DecisionType.cs
@@ -2,9 +2,16 @@
 {
     public enum DecisionType
     {
+        /// <summary>Awaiting Decision From Approver</summary>
         AwaitingDecision = 0,
+
+        /// <summary>Action Approved</summary>
         Approved = 1,
+
+        /// <summary>Action Rejected</summary>
         Rejected = 2,
+
+        /// <summary>Approver Invalidated</summary>
         Invalidated = 3,
     }
 }
